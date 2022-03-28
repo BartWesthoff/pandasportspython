4f19ab98ec6f8d4838a68fafc46b797b98960d1c
bartw <bartwesthoff2002@gmail.com>
2022-03-28 22:04:10 +0200

    added uploading to cloud funcionality

---
+++ classes/cloudfile.py
@@ -0,0 +1,10 @@
+class CloudFile:
+
+    def __init__(self, id, name, parents, size, mime_type, time):
+        self.id = id,
+        self.name = name,
+        self.parents = parents,
+        self.size = size,
+        self.mime_type = mime_type,
+        self.time = time
+
