beacc3a329ee5fe70bbf78b972d6d22336b69056
bartw <bartwesthoff2002@gmail.com>
2022-04-24 16:08:32 +0200

    rework

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from pipeline.pipeline import Pipeline
 from pipeline.steps.input.input import Input
 from pipeline.steps.preprocessors.videopreprocessor import VideoPreProcessor
 from pipeline.steps.embedder.videoembedder import VideoEmbeder
+from pipeline.steps.training.videoTrainer import VideoTrainer
 from pipeline.utils.utils import Utils
 from sklearn import svm
 
 if __name__ == "__main__":
-    VideoEmbeder().process("school video.mp4")
+    VideoTrainer().process("school video.mp4")
 
 
 
--- pipeline/steps/training/videoTrainer.py
+++ pipeline/steps/training/videoTrainer.py
@@ -1,40 +1,31 @@
+import os
 import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
 from pipeline.steps.training.training import Training
 from pipeline.utils.utils import Utils
-import os
 
-# Using linear regression because the datapoints have numerical values (xyz coords of joints) that need to be in proportion to one another. Makes linreg a good choice
 
 class VideoTrainer(Training):
-#    def process(self, data, labels):
-        #data_train, data_test, labels_train, labels_test = train_test_split(
-        #    data, labels, test_size=0.25, random_state=42)
 
-        #LR = LinearRegression().fit(data, labels)
-        #LR = LinearRegression().fit(data_train, labels_train)
-
-        #prediction = LR.score(data, labels)
-        #print(prediction)
-
-    def process(self, data):
+    def process(self, data: list):
+        """gets array of video's"""
+        """return fitted model"""
         labels = []
         utils = Utils()
         allFiles = os.listdir(utils.datafolder)
 
-        for bestand in allFiles:
-            print(bestand)
-            if "positive" in bestand:
-                labels.append(1)
-            elif "negative" in bestand:
-                labels.append(0)
+        for file in allFiles:
+            kind = file.split('_')[0]
+            if kind in ["positive", "negative"]:
+                labels.append(kind)
             else:
                 raise Exception("False names!")
-        
 
+        if len(labels) != len(data):
+            raise Exception("Length of data and labels do not match!")
         model = Utils.define_model()
         data = np.array(data)
         data = np.reshape(data, (len(labels), 137, 99))
         fitted = model.fit(data, np.array(labels), epochs=2, batch_size=2, verbose=1)
         Utils.saveObject(fitted, "fittedSequentialTestModel")
+        print(type(fitted))
+        return fitted
