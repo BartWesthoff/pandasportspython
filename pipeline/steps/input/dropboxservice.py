a8803592c67d6beba5982fc918117eb362b3bc97
bartw <bartwesthoff2002@gmail.com>
2022-06-06 22:59:25 +0200

    credentials van de dropboxservice kan nu ververst worden

--- pipeline/steps/input/dropboxservice.py
+++ pipeline/steps/input/dropboxservice.py
@@ -1,6 +1,7 @@
+import json
 import os
 import pickle
-
+import requests
 import dropbox
 
 from classes.cloudfile import CloudFile
@@ -64,3 +65,28 @@ class DropBoxService(Input):
                 metadata, res = self.service.sharing_get_shared_link_file(url=self.shared_link.url,
                                                                           path="/" + name)
                 f.write(res.content)
+    def refresh_token(self):
+        app_key = "7yiemxlek5s846k"
+        app_secret = "g70t3hljceheq04"
+
+        # build the authorization URL:
+        authorization_url = "https://www.dropbox.com/oauth2/authorize?client_id=%s&response_type=code" % app_key
+
+        # send the user to the authorization URL:
+
+        print(authorization_url)
+
+        # get the authorization code from the user:
+        authorization_code = input('Enter the code:\n')
+
+        # exchange the authorization code for an access token:
+        token_url = "https://api.dropboxapi.com/oauth2/token"
+        params = {
+            "code": authorization_code,
+            "grant_type": "authorization_code",
+            "client_id": app_key,
+            "client_secret": app_secret
+        }
+        r = requests.post(token_url, data=params)
+        res = json.loads(r.text)
+        Utils().saveObject(res['access_token'], "data/credentials/acces_token.pickle")
